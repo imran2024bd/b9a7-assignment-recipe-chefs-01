c45dd5649600a9279d22c11ecc40ab213b3e76cc
imran2024bd <imranitbd34@gmail.com>
2025-02-23 17:46:54 +0600

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
   
@@ -7,8 +8,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-3xl bg-violet-500 rounded-lg items-center p-4'> Food Recipe Chefs React</h1>
-      
+      {/* <h1 className='text-3xl bg-violet-500 rounded-lg items-center p-4'> Food Recipe Chefs React</h1> */}
+      <Header></Header>
       
     </>
   )
