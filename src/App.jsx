1fdb4e92f9fbe1425869c70da04af03cf7deed32
imran2024bd <imranitbd34@gmail.com>
2025-02-27 14:29:47 +0600

    Header and cover completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 
 import './App.css'
+import Cover from './components/Cover/Cover'
 import Header from './components/Header/Header'
 
 function App() {
-  
+
 
   return (
     <>
-      
-      {/* <h1 className='text-3xl bg-violet-500 rounded-lg items-center p-4'> Food Recipe Chefs React</h1> */}
-      <Header></Header>
-      
+
+      <div className='max-w-full'>
+        <Header></Header>
+        <Cover></Cover>
+      </div>
+
     </>
   )
 }
---
+++ src/components/Cover/Cover.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './Cover.css'
+
+const Cover = () => {
+    
+    return (
+        <div className='bg-image text-white mt-10 text-center py-20 mx-auto'>
+            <h1 className='text-6xl font-bold text-center w-2/3 mx-auto '>Discover an exceptional cooking class tailored for you!</h1>
+
+            <p className='py-6'>Discover delicious and nutritious vegetarian recipes perfect for any meal.</p>
+
+            <div className='flex justify-center space-x-10 py-6 text-2xl'>
+                <button className='bg-fuchsia-700 rounded-2xl text-center p-4'>Explore Now</button>
+                <button className='bg-green-600 rounded-2xl text-center p-4'>Our Feedback</button>
+            </div>
+        </div>
+        
+        
+    );
+};
+
+export default Cover;
