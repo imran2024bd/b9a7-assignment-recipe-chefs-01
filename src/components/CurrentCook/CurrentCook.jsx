d9590a0e3d7cb70276ed38d1e5b5ef798d3bed8b
imran2024bd <imranitbd34@gmail.com>
2025-03-11 23:15:50 +0600

    Total Time functionality start with reduce () method

--- src/components/CurrentCook/CurrentCook.jsx
+++ src/components/CurrentCook/CurrentCook.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 
 const CurrentCook = ({ currentCook, idx }) => {
-    // console.log(currentCook);
+    console.log(currentCook);
 
     return (
         <div >
             {
-                currentCook.map(cookObj => (
+                currentCook.map((cookObj , id) =>(
                     <div className="flex justify-around items-center m-12 bg-[#28282808] rounded-lg text-xs text-center space-x-6">
                         <p> {idx + 1} </p>
                         <h1>{cookObj.recipe_name}</h1>
@@ -18,17 +18,28 @@ const CurrentCook = ({ currentCook, idx }) => {
             }
 
             {/* Total Time minutes Section */}
-
+            
             {
+            currentCook.reduce((sum , obj) => {
+                    // console.log(sum + obj.preparing_time );
+                    <div className=" flex justify-end bg-red-500">
+                        <p>Total Time = {sum + obj.preparing_time}  minutes </p>
+                        <p>Total Calories = {sum + obj.calories} calories</p>
+                    </div>
+                },0)
+                }
+
+
+            {/* {
                 currentCook.map(totalCurrent => (
-                    <div  >
+                    <div>
                         <p>Total Time =
                             ${totalCurrent.preparing_time} minutes</p>
                         <p>Total Calories =
                             ${totalCurrent.calories} calories</p>
                     </div>
                 ))
-            }
+            } */}
 
         </div>
     );
