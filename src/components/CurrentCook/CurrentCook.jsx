e1c8e495b3c2f9b873444bee90716c220ac34785
imran2024bd <imranitbd34@gmail.com>
2025-03-12 00:58:12 +0600

    Total Time minutes Section done

--- src/components/CurrentCook/CurrentCook.jsx
+++ src/components/CurrentCook/CurrentCook.jsx
@@ -1,7 +1,15 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const CurrentCook = ({ currentCook, idx }) => {
     console.log(currentCook);
+
+    const [items, setItems] =useState(currentCook)
+    console.log(items);
+
+    const totalTime = items.reduce((sum , obj) => sum + obj.preparing_time , 0)
+    // const totalPrice = items.reduce((sum, obj) => sum + obj.price, 0);
+    console.log(totalTime);
 
     return (
         <div >
@@ -18,8 +26,21 @@ const CurrentCook = ({ currentCook, idx }) => {
             }
 
             {/* Total Time minutes Section */}
-            
+
             {
+                <div className=" flex justify-items-end">
+                <h1>Total Time: {totalTime} minutes</h1>
+                <ul>
+                  {items.map((item) => (
+                    <li key={item.id}>
+                      Total calories {item.id}: {item.calories} calories
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            } 
+
+            {/* {
             currentCook.reduce((sum , obj) => {
                     // console.log(sum + obj.preparing_time );
                     <div className=" flex justify-end bg-red-500">
@@ -27,7 +48,7 @@ const CurrentCook = ({ currentCook, idx }) => {
                         <p>Total Calories = {sum + obj.calories} calories</p>
                     </div>
                 },0)
-                }
+                } */}
 
 
             {/* {
